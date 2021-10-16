667e6323a884f3a8056b5f07870ff6348fb5af74
Victoria <victoria.fischer@cix.csi.cuny.edu>
2021-10-16 18:54:13 -0400

    arbitration settlement

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,9 @@ import labor from '../images/laborleader-cert.jpg';
 import service from '../images/service-cert.jpg';
 import training from '../images/training-cert.jpg';
 import proclamation from '../images/proclomation.JPG';
+import fred from '../images/fred.JPG';
+import ian from '../images/ian.jpg';
+import arbitration from '../images/arbitration.jpeg';
 
 function Home() {
     return (
@@ -32,8 +35,8 @@ function Home() {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="m-auto">
                             <Nav.Link className='navb2' style={{color:"#3a5796"}} href="#about">ABOUT FRED</Nav.Link>
-                            <Nav.Link className='navb2' style={{color:"#3a5796"}} href="#arbitration">ARBITRATION</Nav.Link>
                             <Nav.Link className='navb2' style={{color:"#3a5796"}} href="#vp">ABOUT IAN</Nav.Link>
+                            <Nav.Link className='navb2' style={{color:"#3a5796"}} href="#arbitration">GHIRALDI SETTLEMENT</Nav.Link>
                             {/* <NavDropdown className='navb2' style={{color:"#3a5796"}} title={
                                         <span className="my-auto" style={{color:"#3a5796"}}></span>
                                         } id="collasible-nav-dropdown">
@@ -67,8 +70,10 @@ function Home() {
             
             <div id='about'>
                 <h3 className='title'>FRED FISCHER, PRESIDENT (LOCAL 231)</h3>
-                <img className='profile' src={profile}></img>
+                {/* <img className='profile' src={profile}></img> */}
                 <hr class="solid2"></hr>
+                <img className='fred' src={fred} alt='Fred C Fischer'></img>
+                <br></br><br></br>
                 <p>
                     Fred has exceptional leadership experience within the American Postal Workers Union. <br></br><br></br>
                     During his presidency from 2004-2006, Fred had accomplished: 
@@ -151,16 +156,6 @@ function Home() {
                         <div class="middle">
                             <div class="text">Fred's Proclamation - Awarded April 30, 2009</div>
                         </div>
-                    </div>
-
-            
-
-            <hr class="solid"></hr>
-
-            <div id='arbitration'>
-                <h3 className='title'>ARBITRATION</h3>
-                <hr class="solid2"></hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pretium dui. Etiam sed mauris laoreet, tincidunt dui at, viverra tortor. Etiam lacinia, nisl quis sollicitudin euismod, erat lacus rhoncus risus, ut consectetur ipsum mauris sed lorem. Fusce pharetra ornare velit sit amet aliquet. Duis in placerat mauris, sit amet blandit metus. Aliquam volutpat neque ac nunc posuere dictum. Curabitur porta efficitur mauris, quis mollis tellus malesuada vitae. Suspendisse nec dolor non odio ultricies aliquet luctus id quam. Curabitur imperdiet pulvinar nibh. Proin nec felis finibus, cursus justo nec, bibendum ex. Vivamus risus erat, finibus ac viverra in, consectetur et massa. Mauris augue diam, tempus eget dolor et, aliquet cursus nisi. Ut condimentum cursus dictum. Praesent dictum condimentum tempor. In luctus interdum est ut interdum. Curabitur enim justo, sagittis ac hendrerit sed, condimentum in arcu. Etiam vel vehicula quam. Nunc nec eleifend orci. Vivamus et odio hendrerit, egestas magna molestie, luctus tellus. Aenean placerat luctus mi sit amet vulputate. Cras malesuada bibendum neque, sed interdum purus faucibus nec. Maecenas venenatis leo at nisi malesuada tempus. Duis fermentum tortor erat, eget lacinia elit gravida eu. Nullam sagittis nibh lacus, quis ullamcorper urna auctor bibendum.</p>
             </div>
 
             <hr class="solid"></hr>
@@ -168,13 +163,74 @@ function Home() {
             <div id='vp'>
                 <h3 className='title'>IAN FISHER, VICE PRESIDENT (LOCAL 231)</h3>
                         <div>
-                            <img className='profile' src={profile}></img>
                             <hr class="solid2"></hr>
+                            <img className='ian' src={ian} alt='Ian Fisher'></img>
+                            <br /><br />
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed pretium dui. Etiam sed mauris laoreet, tincidunt dui at, viverra tortor. Etiam lacinia, nisl quis sollicitudin euismod, erat lacus rhoncus risus, ut consectetur ipsum mauris sed lorem. Fusce pharetra ornare velit sit amet aliquet. Duis in placerat mauris, sit amet blandit metus. Aliquam volutpat neque ac nunc posuere dictum. Curabitur porta efficitur mauris, quis mollis tellus malesuada vitae. Suspendisse nec dolor non odio ultricies aliquet luctus id quam. Curabitur imperdiet pulvinar nibh. Proin nec felis finibus, cursus justo nec, bibendum ex. Vivamus risus erat, finibus ac viverra in, consectetur et massa. Mauris augue diam, tempus eget dolor et, aliquet cursus nisi. Ut condimentum cursus dictum. Praesent dictum condimentum tempor. In luctus interdum est ut interdum. Curabitur enim justo, sagittis ac hendrerit sed, condimentum in arcu. Etiam vel vehicula quam. Nunc nec eleifend orci. Vivamus et odio hendrerit, egestas magna molestie, luctus tellus. Aenean placerat luctus mi sit amet vulputate. Cras malesuada bibendum neque, sed interdum purus faucibus nec. Maecenas venenatis leo at nisi malesuada tempus. Duis fermentum tortor erat, eget lacinia elit gravida eu. Nullam sagittis nibh lacus, quis ullamcorper urna auctor bibendum.</p>
                         </div>
-
             </div>
 
+
+            
+
+            <hr class="solid"></hr>
+
+            <div id='arbitration'>
+                <h3 className='title'>GHIRALDI SETTLEMENT</h3>
+                <hr class="solid2"></hr>
+                <p>On January 12, 2070, then-President Gerry Ghiraldi signed a settlement for grievance 
+                    ending in 6097 for <b>no money</b>.</p>
+                <br />
+                <p className='left'>
+                    January 12, 2010
+                    <br />
+                    Gerald Ghiraldi
+                    <br />
+                    President
+                    <br />
+                    APWU
+                    <br />
+                    550 Manor Road
+                    <br />
+                    Staten Island, NY 10314-
+                </p>
+                <br />
+                <p className='right'>Re: B00V-4B-C 06256097
+                    <br />
+                    CLASS ACTION 06NA14
+                    <br />
+                    STATEN ISLAND, NY 10314-9998
+                </p>
+                <p className='numbers'><i>"This is in reference to our discussion held on Wednesday, December 18, 2009 on the above-captioned
+                    grievance resulting in the following mutually agreed upon remedy, which, it is understood, is without 
+                    precedent and shall not be cited in any other appeal forum or arbitration hearing other than enforcement of
+                    its terms subject to the consistency with the National Agreement.</i></p>
+                <p className='numbers'><i>1. The service agrees to increase the current staffing at Staten Island VMF by adding one LDC 32 position; 
+                    this will increase the current staffing to twelve (12) LDC 32 positions.</i></p>
+                <p className='numbers'><i>2. In-Service Agreement is currently posted and closes on February 9, 2010.</i></p>
+                <p className='numbers'><i>3. The union agrees to withdraw their most recent grievance #____-___-________ dated 
+                    December 2009, in view of the fact that the non-compliance issue has been resolved.</i></p>
+                <p className='numbers'><i>4. By signature of your representative, the above settlement is accepted. "</i></p>
+                <p>Signed Gerald Ghiraldi - 1/12/10</p>
+                <img className='arbitration' src={arbitration} alt='Grievance Settlement'></img>
+                <br /><br />
+                <p className='numbers'><b>The settlement that Gerald Ghiraldi willingly signed for grievance ending in 6097 was for personal gain. 
+                    With this settlement, it is clear that Gerald Ghiraldi does not hesitate to betray the membeers of our Union
+                    for his own self-interest. </b>
+                </p>
+
+                <br />
+                <hr class="solid2"></hr>
+                <br />
+
+                <div className='numbers'>
+                    On top of the selfish settlement Gerald Ghiraldi signed, 
+                </div>
+            </div>
+
+       
+
+           
 
 
 
