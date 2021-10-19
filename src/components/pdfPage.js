ee15e79030fd5e49499a7aa936f15c10b5982e9f
Victoria <victoria.fischer@cix.csi.cuny.edu>
2021-10-19 18:15:03 -0400

    pdf view and endorsement

--- src/components/pdfPage.js
+++ src/components/pdfPage.js
@@ -8,9 +8,6 @@ function pdfPage() {
         <div>
             {/* <Link to="/" className='homeButton'>Go Back</Link> */}
             <div>
-                <object data={arbpdf} className='panel' type="application/pdf">
-                </object>
-
                 <object data={arbpdf} className='panel' type="application/pdf" width="100%" height="800px"> 
                 <p>It appears you don't have a PDF plugin for this browser.
                 No biggie... you can <a href={arbpdf}>click here to
